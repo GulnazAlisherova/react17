00809c734efbf3533bea5b2aa8022e4fc72fd81f
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-10 14:35:17 +0600

    added react dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  useEffect(function(){
+    setName(localStorage.getItem('name'));
+  }), 
+
+  setName(localStorage.getItem('name'));
+
+  function onNameChange({ target }) {
+    //create
+    localStorage.setItem('name', target.value);
+
+    setName(target.name);
+  }
   return (
     <div className="App">
-    
+      <input type="text"
+        value={name}
+        placeholder="Your name"
+        onChange={onNameChange}
+      />
+      <span>Your name is {name}</span>
     </div>
   );
 }
