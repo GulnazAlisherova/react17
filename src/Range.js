95c388ca98047236d629822beac00e1f3764cc6b
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-10 15:51:55 +0600

    added value

--- src/Range.js
+++ src/Range.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 function Range() {
+  const [angle, setAngle] = useState(100);
+  function onAngleChange ({ target }) {
+    setAngle(+target.value);
+  }
+
   const styles = {
     width: "200px",
     height: "200px",
     backgroundColor: "tomato",
+    transform: `rotate(${angle}deg)`,
+    transition: '1s all ease-in',
   }
   return (
     <div className="Range">
       <div style={{ marginBottom: "100px" }}>
-        <input type="range" min="0" max="360" />
+        <input type="range" min="0" max="360"  value={angle} onChange={onAngleChange} />
       </div>
       <div style={styles}></div>
     </div>
